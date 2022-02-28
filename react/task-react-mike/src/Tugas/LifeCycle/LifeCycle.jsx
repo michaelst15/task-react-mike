211eef8177bad5877e7a6fe2cbe54b9be70f4c16
michaelst15 <misuto991@gmail.com>
2022-02-28 21:04:57 +0700

    lifecycle

--- react/task-react-mike/src/Tugas/LifeCycle/LifeCycle.jsx
+++ react/task-react-mike/src/Tugas/LifeCycle/LifeCycle.jsx
@@ -6,54 +6,91 @@ import './Style.css';
 class LifeCycle extends Component {
 
    constructor() {
-       super();
+       super();   
        this.state = {
-           data: [],
-           isLoading: true
+           search: '',
+           data: []
        }
    }
 
+   searching = (val) => {
+      this.setState({search: val.target.value});
+   }
+ 
     Api = () => {
-    axios.get('https://newsapi.org/v2/everything?q=tesla&from=2022-01-24&sortBy=publishedAt&apiKey=1e4d7358c26843d98c618391469576e2') 
+    axios.get(
+        `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=1e4d7358c26843d98c618391469576e2`
+    ) 
         .then(response => {
             this.setState ({
            data: response.data.articles
-            });
+            }
+            );
         })
         .catch(err => {
             this.setState ({
-              err,
-              isLoading: true
-            });         
+              err
+            });                
         })
     }
+
+    // handlerSubmit = (e) => {
+    //     e.preventDefault();
+    //     axios.get(
+    //      `https://newsapi.org/v2/everything?q=${this.state.search}&sortBy=publishedAt&apiKey=1e4d7358c26843d98c618391469576e2`
+    //         ) 
+    //     .then(response => {
+    //         this.setState ({
+    //        data: response.data.articles
+    //         });
+    //     })        
+    //     .catch(err => {
+    //         this.setState ({
+    //           err
+    //         });         
+    //     })
+    // }
 
     componentDidMount() {
         this.Api();
     }
-      
+                
     render() {
-        
+         
         return (
             <div>
                <h2 className='bg-dark text-white'>Berita Terkini</h2>
-               <input className='mt-5' type="text" placeholder='Seacrh' />
                <div className='container'>
+               <input 
+               className='mt-5' 
+               value={this.state.search}
+               type="text" 
+               placeholder='Search' />
+               <button 
+               className='btn btn-dark mt-2' 
+               onChange={this.handlerSubmit}>
+                   Cari
+               </button>
+               </div>
+               <div className='container mt-5'>
                   {this.state.data.map((data) => {
                       return (
-                          <div className='container'>
-                            <div>{data.urlToImage}</div>,
-                            <div>{data.title}</div>,
-                            <div>{data.publishedAt}</div>,
-                            <div>{data.description}</div>
-                          </div>
-                              )
+                          <div className='container bg-dark text-white w-50'>
+                            <img src={data.urlToImage} className='mt-3'></img>
+                                <div className='artikel'>
+                                <div>{data.title}</div>
+                                <div>{data.publishedAt}</div>
+                                <div>{data.description}</div>
+                            </div>
+                                <button className='btn btn-primary mt-2'>Read more</button>
+                            </div>
+                              )  
                           })
                        }
                </div>
             </div>
         )
-    }
+    }           
         
 }
 
