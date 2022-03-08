6e4858101f022cf1f9fe69dc34f8dce1cb5cc735
michaelst15 <misuto991@gmail.com>
2022-03-08 14:39:23 +0700

    myWeb

--- react/task-react-mike/src/Tugas/StylingComponent/web/Header/NavBar/Navbar.jsx
+++ react/task-react-mike/src/Tugas/StylingComponent/web/Header/NavBar/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = ({gambar, button1, button2, button3, button4, button5, title, art
        <img src={gambar} className="w-100" style={{height: 500}}
        />
        <div className="mt-2" style={{marginLeft: 1000, position: "absolute", top: 10}}>
-         <Link className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button1}</Link>
-         <button className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button2}</button>
-         <button className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button3}</button>
-         <button className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button4}</button>
+          <Link to='/MyWeb' className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button1}</Link>
+          <Link to='/IndexCrypto' className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button2}</Link>
+          <Link to='/IndexTech' className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button3}</Link>
+          <Link to='/IndexFinance' className="btn btn-primary fw-bold" style={{marginRight: 10}}>{button4}</Link>
        </div>
        <div className="container">
        <div style={{position: "absolute", top: 80, color: "white", width: 400, fontFamily: "Verdana, Geneva, Tahoma, sans-serif"}}>
--- react/task-react-mike/src/Tugas/StylingComponent/web/Router/Index.jsx
+++ react/task-react-mike/src/Tugas/StylingComponent/web/Router/Index.jsx
@@ -1,15 +1,29 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Crypto from './Tugas/StylingComponent/web/Crypto/'
+import {Router, Switch, Route} from 'react-router-dom';
+import Navbar from '../Header/NavBar/Navbar';
+import IndexTech from '../Tech/IndexTech';
+import IndexCrypto from '../Crypto/IndexCrypto';
+import IndexFinance from '../Finance/IndexFinance';
+import MyWeb from '../Content/MyWeb';
 
 const Index = () => {
     return(
       <div>
           <Router>
-              <Switch>
-                  <Route path='/Crypto'>
-                      <Crypto />
-                  </Route>
-              </Switch>
+              <Navbar />
+                <Switch>
+                    <Route path='/MyWeb'>
+                        <MyWeb /> 
+                    </Route>  
+                    <Route path='/IndexCrypto'>
+                        <IndexCrypto />
+                    </Route> 
+                    <Route path='/IndexTech'>
+                        <IndexTech />
+                    </Route>
+                    <Route path='/IndexFinance'>
+                        <IndexFinance />
+                    </Route>
+                </Switch>
           </Router>
       </div>   
     )
