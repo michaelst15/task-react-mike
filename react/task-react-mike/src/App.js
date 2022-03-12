49ce192a963cf2cba9514ea0d9c2547cbdbcf9de
michaelst15 <misuto991@gmail.com>
2022-03-12 21:01:58 +0700

    redux

--- react/task-react-mike/src/App.js
+++ react/task-react-mike/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './Tugas/app/store';
 import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './Tugas/StylingComponent/web/Home/Home';
-import Crypto from './Tugas/StylingComponent/web/Crypto/Crypto';
-import Tech from './Tugas/StylingComponent/web/Tech/Tech';
-import Finance from './Tugas/StylingComponent/web/Finance/Finance';
-import Penutup from './Tugas/StylingComponent/web/Footer/Penutup';
-import Navigation from './Tugas/StylingComponent/web/Navigation/Navigation';
+import Redux from './Tugas/Redux/Redux';
+// import { BrowserRouter, Routes, Route} from 'react-router-dom';
+// import Home from './Tugas/StylingComponent/web/Home/Home';
+// import Crypto from './Tugas/StylingComponent/web/Crypto/Crypto';
+// import Tech from './Tugas/StylingComponent/web/Tech/Tech';
+// import Finance from './Tugas/StylingComponent/web/Finance/Finance';
+// import Penutup from './Tugas/StylingComponent/web/Footer/Penutup';
+// import Navigation from './Tugas/StylingComponent/web/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App"> 
-      <BrowserRouter>
+    <Provider store={store}>
+      <Redux />
+    </Provider>
+      {/* <BrowserRouter>
        <Navigation/>
         <Routes>
           <Route path='/' element={<Home/>}/>
@@ -20,7 +26,7 @@ function App() {
           <Route path='/Finance' element={<Finance/>}/>
         </Routes>
       </BrowserRouter>
-     <Penutup />
+     <Penutup /> */}
     </div>                            
     );  
   }                                                                                                                                                                                                                                                                                                                  
